016aa08e7b8fd90b80ba9dd56b37da672a321d99
Ahsan Qureshi <71433563+ahsanqureshiofficial@users.noreply.github.com>
2026-01-11 15:20:14 +0500

    Update vpsspecs.sh

--- vpsspecs.sh
+++ vpsspecs.sh
@@ -1,6 +1,8 @@
 echo "----------------------------------------" && \
-echo "TOTAL RAM INSTALLED: $(grep MemTotal /proc/meminfo | awk '{printf "%.2f GB", $2/1024/1024}')" && \
-echo "TOTAL CORES : $(grep -c ^processor /proc/cpuinfo)" && \
+echo "CPU NAME: $(grep -m 1 'model name' /proc/cpuinfo | awk -F: '{print $2}' | sed 's/^[ \t]*//')" && \
+echo "TOTAL CORES: $(grep -c ^processor /proc/cpuinfo)" && \
+echo "TOTAL RAM: $(grep MemTotal /proc/meminfo | awk '{printf "%.2f GB", $2/1024/1024}')" && \
+echo "RAM DETAIL: $(sudo dmidecode --type memory 2>/dev/null | grep -E 'Type:|Speed:' | grep -v 'Unknown' | head -n 2 | awk -F: '{printf $2 " "}' | sed 's/^[ \t]*//')" && \
 echo "AVAIABLE DISK SPACE: $(df -h / | awk 'NR==2 {print $4}')" && \
 RESULTS=$(curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 - --simple 2>/dev/null) && \
 echo "DOWNLOAD SPEED: $(echo "$RESULTS" | grep 'Download' | awk '{print $2 " " $3}')" && \
