d53281ac1d061ec163f54baa9cbd86ed010fdc0c
Ahsan Qureshi <71433563+ahsanqureshiofficial@users.noreply.github.com>
2026-01-11 15:18:17 +0500

    Update vpsspecs.sh

--- vpsspecs.sh
+++ vpsspecs.sh
@@ -2,10 +2,7 @@ echo "----------------------------------------" && \
 echo "TOTAL RAM INSTALLED: $(grep MemTotal /proc/meminfo | awk '{printf "%.2f GB", $2/1024/1024}')" && \
 echo "TOTAL CORES : $(grep -c ^processor /proc/cpuinfo)" && \
 echo "AVAIABLE DISK SPACE: $(df -h / | awk 'NR==2 {print $4}')" && \
-echo -n "TESTING NETWORK SPEED (Upload/Download) " && \
-(while true; do echo -n "."; sleep 1; done) & DOTS_PID=$! && \
-RESULTS=$(curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | bash -c 'if command -v python3 &>/dev/null; then python3 - --simple; elif command -v python &>/dev/null; then python - --simple; elif command -v python2 &>/dev/null; then python2 - --simple; else exit 1; fi' 2>/dev/null) && \
-kill $DOTS_PID &>/dev/null && echo "" && \
+RESULTS=$(curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 - --simple 2>/dev/null) && \
 echo "DOWNLOAD SPEED: $(echo "$RESULTS" | grep 'Download' | awk '{print $2 " " $3}')" && \
 echo "UPLOAD SPEED:   $(echo "$RESULTS" | grep 'Upload' | awk '{print $2 " " $3}')" && \
 echo "----------------------------------------"
