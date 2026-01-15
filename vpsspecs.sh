2f74bbc6515b586b30a1fbfd491beaca3b4d3f49
Ahsan Qureshi <71433563+ahsanqureshiofficial@users.noreply.github.com>
2026-01-15 21:12:27 +0500

    Update vpsspecs.sh

--- vpsspecs.sh
+++ vpsspecs.sh
@@ -1,10 +1,26 @@
+# Check for existing testfile and remove it
+[ -f testfile ] && rm testfile
+
 echo "----------------------------------------" && \
 echo "CPU NAME: $(grep -m 1 'model name' /proc/cpuinfo | awk -F: '{print $2}' | sed 's/^[ \t]*//')" && \
 echo "TOTAL CORES: $(grep -c ^processor /proc/cpuinfo)" && \
 echo "TOTAL RAM: $(grep MemTotal /proc/meminfo | awk '{printf "%.2f GB", $2/1024/1024}')" && \
 echo "RAM DETAIL: $(sudo dmidecode --type memory 2>/dev/null | grep -E 'Type:|Speed:' | grep -v 'Unknown' | head -n 2 | awk -F: '{printf $2 " "}' | sed 's/^[ \t]*//')" && \
 echo "AVAIABLE DISK SPACE: $(df -h / | awk 'NR==2 {print $4}')" && \
-RESULTS=$(curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 - --simple 2>/dev/null) && \
+# Disk Write Speed (creating a 1GB file)
+WRITE_SPEED=$(dd if=/dev/zero of=testfile bs=1G count=1 oflag=dsync 2>&1 | awk '/copied/ {print $NF " " $(NF-1)}') && \
+echo "DISK WRITE SPEED: $WRITE_SPEED" && \
+# Clear Cache and Disk Read Speed
+echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null && \
+READ_SPEED=$(dd if=testfile of=/dev/null bs=1G count=1 2>&1 | awk '/copied/ {print $NF " " $(NF-1)}') && \
+echo "DISK READ SPEED:  $READ_SPEED" && \
+# Clean up testfile
+rm testfile && \
+# Network Speed Tests
+RESULTS=$(curl -sSL raw.githubusercontent.com | python3 - --simple 2>/dev/null) && \
 echo "DOWNLOAD SPEED: $(echo "$RESULTS" | grep 'Download' | awk '{print $2 " " $3}')" && \
 echo "UPLOAD SPEED:   $(echo "$RESULTS" | grep 'Upload' | awk '{print $2 " " $3}')" && \
 echo "----------------------------------------"
+
+
+
